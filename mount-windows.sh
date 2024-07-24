62dc9430b650165e40878b045bd8b6bb1924b83a
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2024-07-24 08:44:23 +0200

    Update mount-windows.sh

--- mount-windows.sh
+++ mount-windows.sh
@@ -1,4 +1,4 @@
-wget 
+wget https://download1073.mediafire.com/wpdrk02iptsgfmO46_RvMQ2IcxBc0vDc6r-m4BslqZjnMMUEdPEnnEHTHjHoAagqjybhTyiyxCGzecdld0cisOm8sx5NnFFJu7C1uOA306-xEp3RTS0-CYy_FLqdzcXUPxKUqn5Nixn6NgkEF9_5LizJ_4IBtYHPVHdPdN4ujw/98tx4doaxqqc5vy/Windows-Server-2022.iso
 umount /dev/sdb1
 dd bs=1M conv=fdatasync if=./proxmox.iso of=/dev/sdb
 reboot
